ddfa4a3aab7ea895abc08c758b821906edf780a4
private <user@example.com>
2016-04-07 15:24:29 +0000

    Add the view to enter a new user

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -5,6 +5,11 @@ class UsersControllerTest < ActionController::TestCase
   test "should get new" do
     get :new
     assert_response :success
+    assert assigns(:user)
+  end
+
+  test "should render the signup view" do
+    get :new
     assert_select "title", "Sign up | #{base_title}"
   end
 
