413dc9094ed98a01455d526cb6fafce7506f91dc
private <user@example.com>
2016-04-10 12:09:15 +0000

    Generate controller with rails generate controller Sessions new

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,11 @@ Rails.application.routes.draw do
   get 'help'    => 'static_pages#help'
   get 'about'   => 'static_pages#about'
   get 'contact' => 'static_pages#contact'
+
+  resources        :users
   get 'signup'  => 'users#new'
-  resources :users
+
+  get 'sessions/new'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
