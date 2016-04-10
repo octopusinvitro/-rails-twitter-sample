add0cca68beedbcdb78cf7c68e0e945fda1f871f
private <user@example.com>
2016-04-10 15:23:46 +0000

    Add fixtures with manually generated password_digest

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,10 @@ class User < ActiveRecord::Base
 
   before_save { email.downcase! }
 
+  def self.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
+
 end
