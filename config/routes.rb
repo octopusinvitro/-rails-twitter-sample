3049184d3a4b7a41d88e704cb9e86a38124b6e01
private <user@example.com>
2016-04-10 12:54:58 +0000

    Add view for the log-in (new session)

--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,11 @@ Rails.application.routes.draw do
   get 'contact' => 'static_pages#contact'
 
   resources        :users
-  get 'signup'  => 'users#new'
+  get 'signup' => 'users#new'
 
-  get 'sessions/new'
+  get    'login'  => 'sessions#new'
+  post   'login'  => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
