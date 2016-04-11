aa0f4b2219d1d41fd7fb2b5c85a1e42ebee170f7
private <user@example.com>
2016-04-11 23:12:10 +0000

    Add integration tests for the remember me checkbox

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -16,4 +16,28 @@ class ActiveSupport::TestCase
     !session[:user_id].nil?
   end
 
+  def log_in_as(user, options = {})
+    integration_test? ?
+      integration_test_login(user, options) :
+      normal_test_login(user)
+  end
+
+  private
+
+    def integration_test?
+      defined?(post_via_redirect)
+    end
+
+    def integration_test_login(user, options)
+      password    = options[:password]    || 'password'
+      remember_me = options[:remember_me] || '1'
+      post login_path, session: { email:       user.email,
+                                  password:    password,
+                                  remember_me: remember_me }
+    end
+
+    def normal_test_login(user)
+      session[:user_id] = user.id
+    end
+
 end
