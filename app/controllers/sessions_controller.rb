8b30a401a4c86fc82f68facb63ba6a86416e8447
private <user@example.com>
2016-04-11 22:23:22 +0000

    Add a check box in the login view to remember the user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -29,7 +29,7 @@ class SessionsController < ApplicationController
 
   def success(user)
     log_in user
-    remember user
+    params[:session][:remember_me] == '1' ? remember(user) : forget(user)
     redirect_to user
   end
 
