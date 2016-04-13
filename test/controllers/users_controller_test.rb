b44032ae2ea9bcc05d93a2a24bb985735ac637ba
private <user@example.com>
2016-04-13 16:12:12 +0000

    Require that users are logged in to update a user

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -2,6 +2,10 @@ require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
 
+  def setup
+    @user = users(:michael)
+  end
+
   test "should get new" do
     get :new
     assert_response :success
@@ -34,6 +38,18 @@ class UsersControllerTest < ActionController::TestCase
       }
     end
     assert_redirected_to user_path(assigns(:user))
+  end
+
+  test "should redirect edit when not logged in" do
+    get :edit, id: @user
+    assert_not flash[:danger].empty?
+    assert_redirected_to login_url
+  end
+
+  test "should redirect update when not logged in" do
+    patch :update, id: @user, user: { name: @user.name, email: @user.email }
+    assert_not flash[:danger].empty?
+    assert_redirected_to login_url
   end
 
 end
