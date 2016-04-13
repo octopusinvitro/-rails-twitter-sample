f792723c6a22b7e1b719c0b9d42ec2f21ed75640
private <user@example.com>
2016-04-13 16:00:06 +0000

    Add missing tests for user controller actions

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -13,32 +13,27 @@ class UsersControllerTest < ActionController::TestCase
     assert_select "title", "Sign up | #{base_title}"
   end
 
-  # test "should get user with an id" do
-  #   get :show, { id: @user.id }
-  #   assert_response :success
-  #   assert assigns(:user)
-  # end
+  test "should get user with an id" do
+    get :show, { id: @user.id }
+    assert_response :success
+    assert assigns(:user)
+  end
 
-  # test "should get the show view for a user" do
-  #   get :show, { id: @user.id }
-  #   assert_select "title", "#{@user.name} | #{base_title}"
-  # end
+  test "should get the show view for a user" do
+    get :show, { id: @user.id }
+    assert_select "title", "#{@user.name} | #{base_title}"
+  end
 
-  # test "should post a new user" do
-  #   assert_difference('User.count') do
-  #   post :create, user: {
-  #     name: 'name', 
-  #     email: 'name@user.example.com', 
-  #     password: 'password', 
-  #     password_confirmation:'password'
-  #   }
-  #   end
-  #   assert_redirected_to article_path(assigns(:user))
-  # end
-
-  # test "should render the signup view" do
-  #   get :new
-  #   assert_select "title", "Sign up | #{base_title}"
-  # end
+  test "should post a new user" do
+    assert_difference('User.count') do
+      post :create, user: {
+        name: 'name',
+        email: 'name@user.example.com',
+        password: 'password',
+        password_confirmation:'password'
+      }
+    end
+    assert_redirected_to user_path(assigns(:user))
+  end
 
 end
