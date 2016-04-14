c461b812d992c14c0f05e3947a23c54c31bc524d
private <user@example.com>
2016-04-14 10:47:35 +0000

    Implement friendly forwarding to previous page after login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -30,7 +30,7 @@ class SessionsController < ApplicationController
   def success
     log_in @user
     params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
-    redirect_to @user
+    redirect_back_or(@user)
   end
 
   def error
--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -37,4 +37,26 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     assert_equal email, @user.email
   end
 
+  test "successful edit with friendly forwarding" do
+    get edit_user_path(@user)
+    assert_equal session[:forwarding_url], root_url.chop + edit_user_path(@user)
+
+    log_in_as(@user)
+    assert_redirected_to edit_user_path(@user)
+    assert_nil session[:forwarding_url]
+
+    name  = "Foo Bar"
+    email = "foo@bar.com"
+    patch user_path(@user), user: { name:  name,
+                                    email: email,
+                                    password:              "",
+                                    password_confirmation: "" }
+    assert_not flash.empty?
+    assert_redirected_to @user
+
+    @user.reload
+    assert_equal name,  @user.name
+    assert_equal email, @user.email
+  end
+
 end
