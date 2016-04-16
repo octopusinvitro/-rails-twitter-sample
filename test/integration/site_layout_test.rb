a5dede6da0c280da8a9e079302c952bd190cab17
private <user@example.com>
2016-04-16 00:03:18 +0000

    Add tests for the layout of a logged-in user

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -10,6 +10,18 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
     assert_select "a[href=?]", signup_path
+    assert_select "a[href=?]", login_path
+  end
+
+  test "layout links for logged in users" do
+    user = users(:archer)
+    log_in_as(user)
+    get root_path
+    assert_template 'static_pages/home'
+    assert_select "a[href=?]", users_path
+    assert_select "a[href=?]", user_path(user)
+    assert_select "a[href=?]", edit_user_path(user)
+    assert_select "a[href=?]", logout_path
   end
 
 end
