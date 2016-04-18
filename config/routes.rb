7157be4a9a57aa664c8c004770053a1c033e4d3c
private <user@example.com>
2016-04-18 22:07:30 +0000

    Add stats and follow/unfollow button in the templates

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,11 @@ Rails.application.routes.draw do
   get 'about'   => 'static_pages#about'
   get 'contact' => 'static_pages#contact'
 
-  resources        :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   get 'signup' => 'users#new'
 
   get    'login'  => 'sessions#new'
@@ -15,6 +19,7 @@ Rails.application.routes.draw do
   resources :account_activations, only: [:edit]
   resources :password_resets,     only: [:new, :create, :edit, :update]
   resources :microposts,          only: [:create, :destroy]
+  resources :relationships,       only: [:create, :destroy]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
